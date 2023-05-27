f1e39dbfc74adcb19d1d4bf3a39523f4ffadecb3
Clydinite <clydinite@gmail.com>
2023-05-27 15:47:50 +0800

    added creator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -77,6 +77,13 @@
 						>Contact</a
 					>
 				</li>
+				<li>
+					<a
+						href="/creator"
+						class="block text-lg rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>Creator</a
+					>
+				</li>
 			</ul>
 		</div>
 	</div>
---
+++ src/routes/creator/+page.svelte
@@ -0,0 +1,42 @@
+<div class="flex flex-col md:flex-row">
+	<div class="md:w-5/6">
+		<p class="mx-12 my-12 md:my-auto text-xl">
+			Hello, I'm 郭彥均 (<a href="https://www.instagram.com/clydinite" class="underline"
+				>@clydinite</a
+			>), the creator of this tiny website. Let me provide you with some insights into its
+			development. The website was constructed using the SvelteKit framework, a powerful tool for
+			building web applications. SvelteKit simplifies the development process, enabling me to focus
+			on creating a responsive interface. To style the website, I utilized Tailwind CSS, a
+			widely-used CSS framework known for its utility-first approach, which helped me achieve a
+			modern and clean design. Additionally, Flowbite was employed to enhance the user experience
+			through the creation of UI components. For the font, I opted for JetBrains Mono, a
+			developer-friendly typeface. JetBrains Mono is a monospaced font that promotes easy
+			readability and is designed specifically for developers.
+			You can find the source code
+			<a class="underline" href="https://github.com/Clydinite/ClubWebsite">here! </a>
+		</p>
+	</div>
+
+	<div class="md:w-1/6">
+		<div>
+			<a href="https://kit.svelte.dev/">
+				<img src="/Svelte-logo.svg" class="h-40 w-40 md:h-24 md:w-24 mx-auto" alt="SvelteKit logo" />
+				<p class="p-3 text-center text-lg">Svelte</p>
+			</a>
+		</div>
+
+		<div>
+			<a href="https://tailwindcss.com/">
+				<img src="/Tailwindcss-logo.svg" class="h-40 w-40 md:h-24 md:w-24 mx-auto" alt="TailwindCSS logo" />
+				<p class="p-3 text-center text-lg">TailwindCSS</p></a
+			>
+		</div>
+
+		<div>
+			<a href="https://flowbite.com/">
+				<img src="/Flowbite-logo.svg" class="h-40 w-40 md:h-24 md:w-24 mx-auto" alt="Flowbite logo" />
+				<p class="p-3 text-center text-lg">Flowbite</p></a
+			>
+		</div>
+	</div>
+</div>
