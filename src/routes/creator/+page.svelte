21ec630801d87d9ccf35c10863a2e87b1f57c76a
Clydinite <clydinite@gmail.com>
2023-06-01 01:01:33 +0800

    added secrets, translated the creator section

--- src/routes/creator/+page.svelte
+++ src/routes/creator/+page.svelte
@@ -1,40 +1,52 @@
-<div class="flex flex-col md:flex-row">
+<div class="flex flex-col md:flex-row mb-24">
 	<div class="md:w-5/6">
+		<img
+			src="https://cdn.discordapp.com/attachments/1107308201969459212/1113504280691888178/image.png"
+			class="mx-auto w-full md:w-5/6 object-cover md:rounded-lg m-12"
+			alt="Code"
+		/>
+
 		<p class="mx-12 my-12 md:my-auto text-xl">
-			Hello, I'm 郭彥均 (<a href="https://www.instagram.com/clydinite" class="underline"
+			你好，我是郭彥均 （<a href="https://www.instagram.com/clydinite" class="underline"
 				>@clydinite</a
-			>), the creator of this tiny website. Let me provide you with some insights into its
-			development. The website was constructed using the SvelteKit framework, a powerful tool for
-			building web applications. SvelteKit simplifies the development process, enabling me to focus
-			on creating a responsive interface. To style the website, I utilized Tailwind CSS, a
-			widely-used CSS framework known for its utility-first approach, which helped me achieve a
-			modern and clean design. Additionally, Flowbite was employed to enhance the user experience
-			through the creation of UI components. For the font, I opted for JetBrains Mono, a
-			developer-friendly typeface. JetBrains Mono is a monospaced font that promotes easy
-			readability and is designed specifically for developers.
-			You can find the source code
-			<a class="underline" href="https://github.com/Clydinite/ClubWebsite">here! </a>
+			>），就是我做這個網站的！我來介紹一下我是用什麼東西來做這個網站的吧，我是用SvelteKit框架，它有很多超酷的功能，像是你可以把HTML、CSS、JavaScript全部放到同一個檔案裡面，而且可以不用寫很多boilerplate的程式，它都幫你處理好了！還有，我用了Tailwind
+			CSS，俗話說得好，「最好解決CSS的方法，就是不要解決它」，用這個就可以遠離CSS了。用Tailwind CSS就很像在用inline
+			style，但好處是不用再幫每個element命名了！我覺得用過就回不去了。我還用了Flowbite，它是一個已經設計好的Tailwind CSS的模版，可以改成自己的設計。對了，我的字型是用Jetbrains
+			Mono，以及Noto Sans TC，我覺得看起來很舒服，而且這兩款字型在Google
+			Fonts都可以找得到喔。如果你想要看這個網站的程式，我有開放原始碼，點<a class="underline" href="https://github.com/Clydinite/ClubWebsite">這裡</a>就可以看得到喔！
 		</p>
 	</div>
 
 	<div class="md:w-1/6">
-		<div>
+		<div class="my-4">
 			<a href="https://kit.svelte.dev/">
-				<img src="/Svelte-logo.svg" class="h-40 w-40 md:h-24 md:w-24 mx-auto" alt="SvelteKit logo" />
+				<img
+					src="/Svelte-logo.svg"
+					class="h-40 w-40 md:h-24 md:w-24 mx-auto"
+					alt="SvelteKit logo"
+				/>
 				<p class="p-3 text-center text-lg">Svelte</p>
 			</a>
 		</div>
 
-		<div>
+		<div class="my-4">
 			<a href="https://tailwindcss.com/">
-				<img src="/Tailwindcss-logo.svg" class="h-40 w-40 md:h-24 md:w-24 mx-auto" alt="TailwindCSS logo" />
-				<p class="p-3 text-center text-lg">TailwindCSS</p></a
+				<img
+					src="/Tailwindcss-logo.svg"
+					class="h-40 w-40 md:h-24 md:w-24 mx-auto"
+					alt="Tailwind CSS logo"
+				/>
+				<p class="p-2 text-center text-lg">Tailwind CSS</p></a
 			>
 		</div>
 
-		<div>
+		<div class="my-8">
 			<a href="https://flowbite.com/">
-				<img src="/Flowbite-logo.svg" class="h-40 w-40 md:h-24 md:w-24 mx-auto" alt="Flowbite logo" />
+				<img
+					src="/Flowbite-logo.svg"
+					class="h-40 w-40 md:h-24 md:w-24 mx-auto scale-90"
+					alt="Flowbite logo"
+				/>
 				<p class="p-3 text-center text-lg">Flowbite</p></a
 			>
 		</div>
---
+++ src/routes/secrets/+page.svelte
@@ -0,0 +1,5 @@
+<img
+	class="mx-auto"
+	src="https://cdn.discordapp.com/attachments/1107308201969459212/1113503357659779162/CodeTogether.png"
+	alt="QRCode"
+/>
